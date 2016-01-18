355263e41abf5274c413ae7a56e7bf7310f3d163
mladen-dvojkovic <mladen.dvojkovic@sb.t-com.hr>
2016-01-18 02:24:51 +0100

    Iznajmljivanje automobila
    Skicirano, napravljene tablice, izlistavanje tablica;
    
    Nastavljam sa unosom, izmjenama i brisanjem

---
+++ heron_najam/Iznajmljivanje.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace heron_najam
+{
+    partial class Iznajmljivanje
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.autoDataSet = new heron_najam.autoDataSet();
+            this.Pregled = new System.Windows.Forms.Label();
+            this.listaIznajmljivanja = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.autoDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // autoDataSet
+            // 
+            this.autoDataSet.DataSetName = "autoDataSet";
+            this.autoDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // Pregled
+            // 
+            this.Pregled.AutoSize = true;
+            this.Pregled.Location = new System.Drawing.Point(25, 27);
+            this.Pregled.Name = "Pregled";
+            this.Pregled.Size = new System.Drawing.Size(75, 13);
+            this.Pregled.TabIndex = 0;
+            this.Pregled.Text = "Pregled osoba";
+            // 
+            // listaIznajmljivanja
+            // 
+            this.listaIznajmljivanja.FormattingEnabled = true;
+            this.listaIznajmljivanja.Location = new System.Drawing.Point(28, 71);
+            this.listaIznajmljivanja.Name = "listaIznajmljivanja";
+            this.listaIznajmljivanja.Size = new System.Drawing.Size(521, 277);
+            this.listaIznajmljivanja.TabIndex = 1;
+            // 
+            // Iznajmljivanje
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(585, 412);
+            this.Controls.Add(this.listaIznajmljivanja);
+            this.Controls.Add(this.Pregled);
+            this.Name = "Iznajmljivanje";
+            this.Text = "Iznajmljivanje";
+            this.Load += new System.EventHandler(this.Iznajmljivanje_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.autoDataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private autoDataSet autoDataSet;
+        private System.Windows.Forms.Label Pregled;
+        private System.Windows.Forms.ListBox listaIznajmljivanja;
+    }
+}
+
---
+++ heron_najam/Iznajmljivanje.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace heron_najam
+{
+    public partial class Iznajmljivanje : Form
+    {
+        SqlConnection connection;
+        string connectionString;
+
+        public Iznajmljivanje()
+        {
+            InitializeComponent();
+
+            connectionString = ConfigurationManager.ConnectionStrings["heron_najam.Properties.Settings.autoConnectionString"].ConnectionString;
+        }
+        
+        private void Iznajmljivanje_Load(object sender, EventArgs e)
+        {
+            Izlistaj();
+        }
+
+        private void Izlistaj()
+        {
+            using (connection = new SqlConnection(connectionString))
+            using (SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM osobe", connection))
+            {
+
+                DataTable pregled = new DataTable();
+                adapter.Fill(pregled);
+
+                listaIznajmljivanja.DisplayMember = "ime_prezime";
+                listaIznajmljivanja.ValueMember = "Id";
+                listaIznajmljivanja.DataSource = pregled;
+            }
+        }
+
+        private void listaIznajmljivanja_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show(listaIznajmljivanja.SelectedValue.ToString());
+        }
+    }
+}
